4201047ce022a4cf9813f827be3cf6339fb84a49
feyzanur <121286488+dfeyzanur@users.noreply.github.com>
2024-03-04 14:50:43 +0300

    sayi_tahmin_oyunu

---
+++ tahmin_oyunu/tahmin_oyunu/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace tahmin_oyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonBasla = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonTahmin = new System.Windows.Forms.Button();
+            this.labelMesaj = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonBasla
+            // 
+            this.buttonBasla.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonBasla.Location = new System.Drawing.Point(25, 22);
+            this.buttonBasla.Name = "buttonBasla";
+            this.buttonBasla.Size = new System.Drawing.Size(96, 40);
+            this.buttonBasla.TabIndex = 0;
+            this.buttonBasla.Text = "Başla";
+            this.buttonBasla.UseVisualStyleBackColor = true;
+            this.buttonBasla.Click += new System.EventHandler(this.buttonBasla_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(304, 199);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(142, 45);
+            this.textBox1.TabIndex = 1;
+            // 
+            // buttonTahmin
+            // 
+            this.buttonTahmin.Location = new System.Drawing.Point(362, 273);
+            this.buttonTahmin.Name = "buttonTahmin";
+            this.buttonTahmin.Size = new System.Drawing.Size(75, 40);
+            this.buttonTahmin.TabIndex = 2;
+            this.buttonTahmin.Text = "Tahmin Et";
+            this.buttonTahmin.UseVisualStyleBackColor = true;
+            this.buttonTahmin.Click += new System.EventHandler(this.buttonTahmin_Click_1);
+            // 
+            // labelMesaj
+            // 
+            this.labelMesaj.AutoSize = true;
+            this.labelMesaj.Location = new System.Drawing.Point(301, 45);
+            this.labelMesaj.Name = "labelMesaj";
+            this.labelMesaj.Size = new System.Drawing.Size(0, 16);
+            this.labelMesaj.TabIndex = 3;
+            this.labelMesaj.Visible = false;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.LinkColor = System.Drawing.Color.Red;
+            this.linkLabel1.Location = new System.Drawing.Point(626, 228);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(104, 16);
+            this.linkLabel1.TabIndex = 4;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "5 Hakkınız Kaldı!";
+            this.linkLabel1.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(629, 199);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            this.label1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.labelMesaj);
+            this.Controls.Add(this.buttonTahmin);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonBasla);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonBasla;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonTahmin;
+        private System.Windows.Forms.Label labelMesaj;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ tahmin_oyunu/tahmin_oyunu/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tahmin_oyunu
+{
+    public partial class Form1 : Form
+    {
+       
+        public Form1()
+        {
+            
+
+            InitializeComponent();
+        }
+
+        private void buttonBasla_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            hak = 5;
+            labelMesaj.Text = "SAYI TAHMİN OYUNUNA HOŞGELDİNİZ! \n 1-20 ARASI BİR SAYI GİRİNİZ!";
+            Random random = new Random();
+            int sayi = random.Next(0, 20);
+
+            label1.Text = sayi.ToString();
+            linkLabel1.Text = (hak.ToString()+"Hakkınız Var!");
+            linkLabel1.Visible = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            labelMesaj.Visible = true;
+          
+
+        }
+
+        int hak = 5;
+        int tahminfonk(int sayi)
+        {
+            int tahmin = Convert.ToInt32(label1.Text);
+
+            if (hak == 0)
+            {
+                labelMesaj.Text = "Kaybettiniz!";
+            }
+            else
+            {
+                if (sayi == tahmin)
+                {
+                    MessageBox.Show("KAZANDINIZ!");
+                    labelMesaj.Text = "Tekrar oynamak için Başla butonuna tıklayınız!";
+
+
+                }
+
+                else
+                {
+                    if (sayi < tahmin)
+                    {
+                        labelMesaj.Text = "YANLIŞ TAHMİN TEKRAR DENEYİNİZ! \n DAHA BÜYÜK!";
+                        hak--;
+                    }
+                    else
+                    {
+                        labelMesaj.Text = "YANLIŞ TAHMİN TEKRAR DENEYİNİZ!\n DAHA KÜÇÜK";
+                        hak--;
+                    }
+                    
+
+
+                }
+            }
+           
+            return tahmin;
+        }
+
+        private void buttonTahmin_Click_1(object sender, EventArgs e)
+        {
+            
+            
+            int sayi=Convert.ToInt32(textBox1.Text);
+            if(sayi>0 && sayi<20)
+            tahminfonk(sayi);
+            else
+            {
+                MessageBox.Show("1-20 arası bir sayi giriniz!");
+            }
+           
+            linkLabel1.Text=(hak.ToString()+" Hakkınız kaldı!");
+
+
+        }
+    }
+}
